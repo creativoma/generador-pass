18921d79e6a0944c4c7d51522c49ce46383f410f
Mariano Álvarez <ma.marianoalvarez@gmail.com>
2023-04-12 09:44:58 +0200

    Proyecto terminado

---
+++ README.md
@@ -0,0 +1,19 @@
+# Generador de passwords
+
+Es un proyecto personal que hice para generar passwords de forma aleatoria.
+
+## Pantallazo
+
+![screenshot](./public/screenshot.png)
+
+## Colaboración
+
+Si queres colaborar con el proyecto, podes hacerlo de las siguientes maneras:
+
+-   Haciendo un fork del proyecto y enviando un pull request.
+-   Creando un issue en el repositorio.
+-   Contactandome por [Twitter](https://twitter.com/dev_creativoma).
+
+## Licencia
+
+MIT License - Copyright (c) 2023 Mariano Álvarez
---
+++ src/App.jsx
@@ -0,0 +1,155 @@
+// Dependencias
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Alert from "@mui/material/Alert";
+import InputAdornment from "@mui/material/InputAdornment";
+import OutlinedInput from "@mui/material/OutlinedInput";
+
+// Variables
+const anchoInput = "w-full md:w-[calc(10%-1rem)]";
+const anchoButton = "w-full md:w-[calc(35%-1rem)]";
+const anchoOutput = "w-full md:w-[calc(55%-1rem)]";
+
+const generate = () => {
+  let length = document.getElementById("length").value;
+  let charset =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    let random = Math.floor(Math.random() * charset.length);
+    password += charset.substring(random, random + 1);
+  }
+  document.getElementById("password").value = password;
+};
+
+const copyPassword = () => {
+  let password = document.getElementById("password").value;
+  let alert = document.getElementById("alert");
+  let alert2 = document.getElementById("alert2");
+
+  if (password.length < 6) {
+    alert.style.opacity = "0";
+    alert2.style.opacity = "1";
+
+    setTimeout(() => {
+      alert2.style.opacity = "0";
+    }, 2000);
+  } else {
+    navigator.clipboard.writeText(password);
+    alert2.style.opacity = "0";
+    alert.style.opacity = "1";
+
+    setTimeout(() => {
+      alert.style.opacity = "0";
+    }, 2000);
+  }
+};
+
+const copyIcon = () => {
+  return '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-copy" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"> <path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M8 8m0 2a2 2 0 0 1 2 -2h8a2 2 0 0 1 2 2v8a2 2 0 0 1 -2 2h-8a2 2 0 0 1 -2 -2z"></path> <path d="M16 8v-2a2 2 0 0 0 -2 -2h-8a2 2 0 0 0 -2 2v8a2 2 0 0 0 2 2h2"></path></svg>';
+};
+
+function App() {
+  return (
+    <div className="w-screen flex justify-center items-center gap-5">
+      <div className="px-12 py-12 sm:px-20 max-w-5xl flex gap-5 flex-wrap justify-evenly">
+        <h1
+          className="text-3xl md:text-4xl font-bold w-full text-center mb-2"
+          style={{ color: "#d22" }}
+        >
+          🗝️ Generador de contraseñas
+        </h1>
+        <p className="text-xs w-full text-center mb-6">
+          {" "}
+          <strong>Disclaimer:</strong> No me hago responsable del uso que se le
+          de a esta herramienta. Es un proyecto personal y no tiene fines
+          comerciales. No almaceno ni recojo datos de ningún tipo. Y no me hago
+          responsable de la seguridad de las contraseñas generadas.{" "}
+        </p>
+
+        <TextField
+          id="length"
+          defaultValue="6"
+          InputProps={{
+            inputProps: { min: 6, max: 36 },
+            onFocus: {
+              color: "#d22",
+            },
+          }}
+          variant="outlined"
+          className={anchoInput}
+          type="number"
+        />
+        <Button
+          variant="contained"
+          className={anchoButton}
+          style={{ backgroundColor: "#d22", color: "#fff", paddingTop: "1rem", paddingBottom: "1rem"}}
+          onClick={generate}
+        >
+          Generar contraseña
+        </Button>
+        <OutlinedInput
+          id="password"
+          endAdornment={
+            <InputAdornment position="end">
+              {" "}
+              <span dangerouslySetInnerHTML={{ __html: copyIcon() }} />{" "}
+            </InputAdornment>
+          }
+          InputProps={{
+            readOnly: true,
+          }}
+          style={{ cursor: "pointer" }}
+          variant="outlined"
+          className={anchoOutput}
+          onClick={copyPassword}
+        />
+        <Alert
+          id="alert"
+          style={{
+            position: "absolute",
+            top: "20px",
+            left: "50%",
+            transform: "translateX(-50%)",
+            opacity: "0",
+            transition: "all 0.150s ease",
+            width: "max-content",
+          }}
+          severity="success"
+          color="success"
+        >
+          Contraseña copiada con éxito!
+        </Alert>
+        <Alert
+          id="alert2"
+          style={{
+            position: "absolute",
+            top: "20px",
+            left: "50%",
+            transform: "translateX(-50%)",
+            opacity: "0",
+            transition: "all 0.150s ease",
+            width: "max-content",
+          }}
+          severity="error"
+          color="error"
+        >
+          Se recomienda una longitud de al menos 6 caracteres
+        </Alert>
+        <p className="text-s w-full text-center mt-6">
+          {" "}
+          Hecho con la mejor 🤙 por{" "}
+          <a
+            href="https://github.com/creativoma"
+            target="_blank"
+            style={{ color: "#d22" }}
+          >
+            Mariano Álvarez
+          </a>{" "}
+          - Copyright © {new Date().getFullYear()}
+        </p>
+      </div>
+    </div>
+  );
+}
+export default App;
